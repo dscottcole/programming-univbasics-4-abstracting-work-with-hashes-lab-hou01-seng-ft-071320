1b5e832cc51b742e02f5140307a5bb99e96ffce0
Daniel Cole <danielscole1@gmail.com>
2020-07-01 16:46:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
     hash = {
-      key => value
+      :name => value
     }
     binding.pry
   hash
